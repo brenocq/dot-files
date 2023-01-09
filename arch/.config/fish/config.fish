fea62c22d1f5c0647b7634e838979c076de9cb42
Breno Cunha Queiroz <breno1423@gmail.com>
2023-01-09 03:19:45 +0100

    Vim spellcheck, clipboard copy, ctags

--- arch/.config/fish/config.fish
+++ arch/.config/fish/config.fish
@@ -8,4 +8,7 @@ if status is-interactive
         fortune computers definitions disclaimer fortunes linux rules-of-acquisition wisdom work zippy paradoxum | cowsay -f $(ls /usr/share/cows/ | shuf -n1)
     end
     alias emsdk_setup ". ~/Programs/emsdk/emsdk_env.fish"
+    alias ppk "sudo nrfconnect --no-sandbox"
+    alias saleae "sudo saleae-logic2 --no-sandbox"
+    alias cpptags "ctags -R --c++-kinds=+p --fields=+iaS --extras=+q ."
 end
