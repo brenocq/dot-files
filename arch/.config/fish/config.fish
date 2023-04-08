b74aad6212727818089a2cd7ede035e14852ec58
Breno Cunha Queiroz <breno1423@gmail.com>
2023-04-08 11:08:53 -0300

    Feat: Vim doxygen & latex

--- arch/.config/fish/config.fish
+++ arch/.config/fish/config.fish
@@ -1,12 +1,15 @@
 set fish_greeting
 if status is-interactive
-    alias wifi "nmtui"
     eval "$(starship init fish)"
     eval "$(jump shell fish)"
 
+    # Show fortune message
     if [ (math (random)'%10') -eq 0 ]
         fortune computers definitions disclaimer fortunes linux rules-of-acquisition wisdom work zippy paradoxum | cowsay -f $(ls /usr/share/cows/ | shuf -n1)
     end
+
+    alias r "ranger"
+    alias wifi "nmtui"
     alias emsdk_setup ". ~/Programs/emsdk/emsdk_env.fish"
     alias ppk "sudo nrfconnect --no-sandbox"
     alias saleae "sudo saleae-logic2 --no-sandbox"
